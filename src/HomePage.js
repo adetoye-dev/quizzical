138e3ffbd35f9121d3957fe6af65e50ccf48668b
Adetoye Adewoye <adetoyeadewoye@gmail.com>
2022-09-26 11:10:34 +0100

    create and style quiz homepage

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,9 +1,13 @@
 import React from "react";
-
+import blobRight from "./assets/home-blob-right.png";
+import blobLeft from "./assets/home-blob-left.png";
 const HomePage = () => {
   return (
-    <div>
-      <h1>HomePage</h1>
+    <div className="home container">
+      <img src={blobRight} className="right-blob" />
+      <h1 className="title">Quizzical</h1>
+      <button className="start-quiz">Start quiz</button>
+      <img src={blobLeft} className="left-blob" />
     </div>
   );
 };
