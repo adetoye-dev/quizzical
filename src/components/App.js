ab3d382c98c000ba94f49dbaf84b982bc84c4c49
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-01-28 18:43:54 +0100

    Move App to component directory and update imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
-import HomePage from "./components/HomePage";
-import QuizPage from "./components/QuizPage";
+import HomePage from "./HomePage";
+import QuizPage from "./QuizPage";
 
 const App = () => {
   const [quiz, setQuiz] = useState(false);
