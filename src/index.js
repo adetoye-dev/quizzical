e0ba33583247008dc8c1baa0395ee339918e857e
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-01-28 18:29:45 +0100

    Import and render userSelect context

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import UserSelect from "./contexts/UserSelect";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <UserSelect>
+      <App />
+    </UserSelect>
   </React.StrictMode>
 );
