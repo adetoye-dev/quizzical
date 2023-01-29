b014f0c8b943074e0c462091032b564ee8241995
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-01-29 16:45:29 +0100

    Create loader

---
+++ src/components/Loader.js
@@ -0,0 +1,10 @@
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div className="loader-spin"></div>
+      <span className="loader-text">Fetching Questions...</span>
+    </div>
+  );
+};
+
+export default Loader;
