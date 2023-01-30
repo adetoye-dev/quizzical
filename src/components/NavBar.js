5f4c111cdf0847332a987be970b62c3c2e2acc30
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-01-30 22:59:14 +0100

    Create NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+
+const NavBar = () => {
+  const [dataAttributes, setDataAttributes] = useState(false);
+
+  const toggleNav = () => {
+    setDataAttributes((prevAttr) => !prevAttr);
+  };
+
+  return (
+    <header className="header-element">
+      <div className="header">
+        <div className="nav-container">
+          <div className="logo">
+            <h2>Quizzical</h2>
+          </div>
+          <nav className="nav">
+            <ul
+              className="primary-navigation"
+              data-visible={dataAttributes}
+              id="primary-navigation"
+            >
+              <div onClick={toggleNav} className="nav-icon cancel-btn">
+                <i className="fas fa-times"></i>
+              </div>
+              <Link to="/" className="nav-link" onClick={toggleNav}>
+                Home
+              </Link>
+            </ul>
+            <div
+              aria-controls="primary-navigation"
+              onClick={toggleNav}
+              className="nav-icon menu-btn"
+            >
+              <i className="fas fa-bars"></i>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
